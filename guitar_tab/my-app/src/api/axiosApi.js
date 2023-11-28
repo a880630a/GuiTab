24ddd5f56484c1b3dde99da96e018038a41da7fe
Xian <dragonball880630@gmail.com>
2023-11-28 23:23:50 +0800

    refactor: IP>修改IP至本地端

--- guitar_tab/my-app/src/api/axiosApi.js
+++ guitar_tab/my-app/src/api/axiosApi.js
@@ -3,7 +3,8 @@ import _axios from "axios"
 const axios = (baseURL) => {
     const instance = _axios.create({
 				//建立一個自定義的axios
-        baseURL: baseURL || 'http://0.0.0.0:8877', //Flask所開的端口位置
+        // baseURL: baseURL || 'http://0.0.0.0:8877', //若使用這個可能導致部分設備無法與後端通訊
+        baseURL: baseURL || 'http://127.0.0.1:8877', //Flask所開的端口位置
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json;charset=UTF-8',
@@ -11,7 +12,7 @@ const axios = (baseURL) => {
             'Access-Control-Allow-methods':'GET,POST',
             'Access-Control-Allow-Private-Network': true
         },
-        timeout: 50000,
+        timeout: 80000,
     });
     return instance
 }
